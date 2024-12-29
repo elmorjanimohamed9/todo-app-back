9df27176ddda7763a1506c60816548822783a34f
Mohamed El Morjani <elmorjanimohamed9@gmail.com>
2024-12-29 18:53:36 +0100

    Create env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/todo-app'),
-    TodoModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), TodoModule],
 })
 export class AppModule {}
