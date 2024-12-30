02e8713b99f724938426c8a207d0422a427ef963
Mohamed El Morjani <elmorjanimohamed9@gmail.com>
2024-12-30 01:43:12 +0100

    configure the Mongoose connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), TodoModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        dbName: 'todo-app',
+      }),
+      inject: [ConfigService],
+    }),
+    TodoModule,
+  ],
 })
 export class AppModule {}
